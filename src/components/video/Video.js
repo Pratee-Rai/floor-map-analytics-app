f497565078cde27f118af714e1e0c449a03e00fb
aravind.emmadishetty <aravind.emmadishetty@acsicorp.com>
2023-04-17 22:24:42 +0530

    Added video component and some style changes

---
+++ src/components/video/Video.js
@@ -0,0 +1,63 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import pauseIcon from "../../images/icons/pause.svg";
+import playIcon from "../../images/icons/play.svg";
+import "./Video.scss";
+
+export default function Video({ children, ...props }) {
+  const [pause, setPause] = useState(false);
+  const [max, setMax] = useState(0);
+  const [progress, setProgress] = useState(100);
+  const videoRef = useRef();
+
+  function handlePlayPause() {
+    if (pause) videoRef.current.play();
+    else videoRef.current.pause();
+    setPause(!pause);
+  }
+
+  const handleTimeUpdate = (e) => {
+    if (!max) setMax(e.target.duration);
+    setProgress(e.target.currentTime);
+  };
+
+  const handleSeekVideo = (e) => {
+    const progress = e.target;
+    const rect = progress.getBoundingClientRect();
+    const pos = (e.pageX - rect.left) / progress.offsetWidth;
+    videoRef.current.currentTime = pos * videoRef.current.duration;
+  };
+
+  return (
+    <div className="video-container">
+      <video
+        {...props}
+        ref={videoRef}
+        onTimeUpdate={handleTimeUpdate}
+        controls={false}
+      >
+        {children}
+      </video>
+      <div className="controls">
+        <div className="progress">
+          <progress
+            id="progress"
+            onClick={handleSeekVideo}
+            value={progress}
+            min="0"
+            max={max}
+          ></progress>
+        </div>
+        <div>
+          <img
+            id="playpause"
+            onClick={handlePlayPause}
+            src={pause ? playIcon : pauseIcon}
+            alt="Play/Pause"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/floor-map-analytics/Heatmap.js
+++ src/floor-map-analytics/Heatmap.js
@@ -3,6 +3,7 @@ import heatmapImg from "../images/heatmap.png";
 import tvLogo from "../images/icons/live-tv.svg";
 import directionsIcon from "../images/icons/directions.svg";
 import areaPath from "../images/icons/area-path.svg";
+import Video from "../components/video/Video";
 export default function Heatmap({ location, area }) {
   const [isWatching, setIsWatching] = useState(false);
 
@@ -28,7 +29,7 @@ export default function Heatmap({ location, area }) {
             {!isWatching ? (
               <>
                 <img src={heatmapImg} alt="Heat Map" height={434} width={572} />
-                <button onClick={handleWatchNowClick}>
+                <button className="play-button" onClick={handleWatchNowClick}>
                   Watch Video <img src={tvLogo} alt="tv logo" />
                 </button>
                 <img
@@ -38,9 +39,12 @@ export default function Heatmap({ location, area }) {
                 />
               </>
             ) : (
-              <video height={434} width={572} controls>
-                <source src={`./media/${area}/output-seg-heat.mp4`} />
-              </video>
+              <Video height={434} width={572} controls autoPlay>
+                <source
+                  src={`./media/${area}/output-seg-heat.mp4`}
+                  type="video/mp4"
+                />
+              </Video>
             )}
           </>
         </div>
