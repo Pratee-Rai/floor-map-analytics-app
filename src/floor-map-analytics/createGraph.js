7051aae19d0658552bcdcc2201dba29df5b314bf
Rai <pratibha.rai@acsicorp.com>
2023-04-19 13:49:54 +0530

    updated graph logics

--- src/floor-map-analytics/createGraph.js
+++ src/floor-map-analytics/createGraph.js
@@ -1,145 +1,148 @@
 import { useEffect, useState } from "react";
 import Papa from "papaparse";
 import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  // Legend,
-  ReferenceLine,
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    ReferenceLine,
 } from "recharts";
 import outputFile from "../data/output-minute.csv";
 
 const CreateGraph = ({ selectedfrequency, selectedTime }) => {
-  const [data, setData] = useState([]);
-  const [dataCopy, setDataCopy] = useState([]);
-  const [totalCount, setTotalCount] = useState(0);
-  const [avgCount, setAvgCount] = useState(0);
-  const [maxCount, setMaxCount] = useState(0);
-  const [formattedData, setFormattedData] = useState();
+    const [data, setData] = useState([]);
+    const [dataCopy, setDataCopy] = useState([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const [avgCount, setAvgCount] = useState(0);
+    const [maxCount, setMaxCount] = useState(0);
+    const [formattedData, setFormattedData] = useState();
+    // const legendData = [{ "id": 1, "text": "Max capacity" }]
 
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch(outputFile);
-      const reader = response.body.getReader();
-      const result = await reader.read();
-      const decoder = new TextDecoder("utf-8");
-      const csv = decoder.decode(result.value);
-      Papa.parse(csv, {
-        delimiter: ",",
-        newline: "",
-        header: true,
-        dynamicTyping: true,
-        skipEmptyLines: true,
-        complete: function (input) {
-          const records = input.data;
-          setData(records);
-        },
-      });
-    }
-    getData();
-  }, []);
-
-  useEffect(() => {
-    let total = 0;
-    if (data?.length !== 0 && data?.length !== undefined && totalCount === 0) {
-      // let dataTocheck = formattedData?.length !== 0 ? formattedData : data;
-      total = data
-        .map(function (a) {
-          return a.people;
-        })
-        .reduce(function (a, b) {
-          return a + b;
-        });
-      let avg = total / data?.length;
-      const max = data
-        .map(function (a) {
-          return a.people;
-        })
-        .reduce(function (prev, current) {
-          return prev > current ? prev : current;
-        });
-      setTotalCount(total);
-      setAvgCount(Math.round(avg));
-      setMaxCount(max + 1);
-    }
-  }, [data, totalCount]);
-
-  useEffect(() => {
-    const timeDigit = selectedfrequency.split(" ")[0];
-    if (timeDigit > 1) {
-      const timeToMs = timeDigit * 60 * 1000;
-      let result = [data[0]];
-      for (let i = 0; i < data?.length; i++) {
-        let prev = data[0];
-        for (let j = 1; j < data?.length; j++) {
-          let current = data[j];
-          let [h1, m1] = prev.timespan.split(":");
-          let [h2, m2] = current.timespan.split(":");
-          let start = new Date(),
-            end = new Date();
-
-          start.setHours(h1);
-          start.setMinutes(m1);
-          end.setHours(h2);
-          end.setMinutes(m2);
-          let diff = end.getTime() - start.getTime();
-
-          if (diff === timeToMs && result.indexOf(current) === -1) {
-            console.log(current);
-            result.push(current);
-          }
+    useEffect(() => {
+        async function getData() {
+            const response = await fetch(outputFile);
+            const reader = response.body.getReader();
+            const result = await reader.read();
+            const decoder = new TextDecoder("utf-8");
+            const csv = decoder.decode(result.value);
+            Papa.parse(csv, {
+                delimiter: ",",
+                newline: "",
+                header: true,
+                dynamicTyping: true,
+                skipEmptyLines: true,
+                complete: function (input) {
+                    const records = input.data;
+                    setData(records);
+                },
+            });
         }
-      }
+        getData();
+    }, []);
 
-      setFormattedData(result);
-      setTotalCount(0);
-      setDataCopy(result);
-    } else  {
-      setDataCopy(data);
-    }
-  }, [selectedfrequency, data, formattedData?.length]);
+    useEffect(() => {
+        let total = 0;
+        if (data?.length !== 0 && data?.length !== undefined && totalCount === 0) {
+            total = data
+                .map(function (a) {
+                    return a.people;
+                })
+                .reduce(function (a, b) {
+                    return a + b;
+                });
+            let avg = total / data?.length;
+            const max = data
+                .map(function (a) {
+                    return a.people;
+                })
+                .reduce(function (prev, current) {
+                    return prev > current ? prev : current;
+                });
+            setTotalCount(total);
+            setAvgCount(Math.round(avg));
+            setMaxCount(max + 1);
+        }
+    }, [data, totalCount]);
 
-  console.log(data);
-  return (
-    <>
-      <AreaChart
-        width={650}
-        height={650 * 0.6}
-        data={dataCopy.length > 1 ? dataCopy : data}
-        margin={{
-          top: 50,
-          right: 20,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="timespan"
-          stroke="#000000"
-          fontSize="10"
-          label={{
-            value: "Time Stamp",
-            position: "insideBottom",
-            fontSize: 14,
-          }}
-        />
-        <YAxis
-          fontSize="10"
-          axisLine={false}
-          label={{ value: "Employee Count", angle: -90, fontSize: 14 }}
-        />
-        <Tooltip />
-        {/* <Legend /> */}
-        <ReferenceLine y={maxCount} stroke="red" strokeDasharray="3 3" />
-        <ReferenceLine y={avgCount} stroke="blue" strokeWidth={3} />
-        <Area type="monotone" dataKey="people" fill="#6ca7f5" />
-      </AreaChart>
-    </>
-  );
+    useEffect(() => {
+        const timeDigit = selectedfrequency.split(" ")[0];
+        if (timeDigit > 1) {
+            const timeToMs = timeDigit * 60 * 1000;
+            let result = [data[0]];
+            for (let i = 0; i < data?.length; i++) {
+                let prev = data[0];
+                for (let j = 1; j < data?.length; j++) {
+                    let current = data[j];
+                    let [h1, m1] = prev.timespan.split(":");
+                    let [h2, m2] = current.timespan.split(":");
+                    let start = new Date(),
+                        end = new Date();
+
+                    start.setHours(h1);
+                    start.setMinutes(m1);
+                    end.setHours(h2);
+                    end.setMinutes(m2);
+                    let diff = end.getTime() - start.getTime();
+
+                    if (diff === timeToMs && result.indexOf(current) === -1) {
+                        result.push(current);
+                    }
+                }
+            }
+
+            setFormattedData(result);
+            setTotalCount(0);
+            setDataCopy(result);
+        } else {
+            setDataCopy([]);
+        }
+    }, [selectedfrequency, data, formattedData?.length]);
+
+    return (
+        <>
+            <AreaChart
+                width={650}
+                height={650 * 0.6}
+                data={dataCopy.length > 1 ? dataCopy : data}
+                margin={{
+                    top: 50,
+                    right: 20,
+                    left: 0,
+                    bottom: 0,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis
+                    dataKey="timespan"
+                    stroke="#000000"
+                    fontSize="10"
+                    label={{
+                        value: "Time Stamp",
+                        position: "insideBottom",
+                        fontSize: 14,
+                    }}
+                />
+                <YAxis
+                    fontSize="10"
+                    axisLine={false}
+                    label={{ value: "Employee Count", angle: -90, fontSize: 14 }}
+                />
+                <Tooltip />
+                <ReferenceLine y={maxCount} stroke="red" strokeDasharray="3 3" strokeWidth={2} label={{value:"Max capacity", position: "top"}} />
+                <ReferenceLine y={avgCount} stroke="blue" strokeWidth={3} label={{value:"Average occupancy", position: "top"}} />
+                {/* <Legend
+                    iconType="circle"
+                    layout="horizontal"
+                    align="right"
+                    verticalAlign="bottom"
+                /> */}
+                <Area type="monotone" dataKey="people" fill="#6ca7f5" />
+            </AreaChart>
+
+        </>
+    );
 };
 
 export default CreateGraph;
--- src/floor-map-analytics/index.js
+++ src/floor-map-analytics/index.js
@@ -1,10 +1,20 @@
 import { useState } from "react";
+import dayjs from 'dayjs';
+import moment from 'moment';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { SingleInputTimeRangeField } from '@mui/x-date-pickers-pro/SingleInputTimeRangeField';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import CreateGraph from "./createGraph";
 import Heatmap from "./Heatmap";
 import "./fma.css";
 
+
 const locationOptions = ["Hyderabad", "Noida", "Pune", "Chennai"];
 const areaOptions = ["Bay Area 1", "Bay Area 2", "Cafeteria"];
+
 const frequencyOptions = [
   "1 min",
   "5 min",
@@ -24,13 +34,33 @@ export default function FloorMapAnalytics() {
   });
   const [time, setTime] = useState(0);
 
+  let currentDateTime = moment().format();
+  let updatedCurrentDateTime = moment(currentDateTime).add(1, 'minute')
+
+  const [customTimeData, setCustomTimeData] = useState([dayjs(currentDateTime), updatedCurrentDateTime])
+  const [value, setValue] = useState('21:58:00-20:10:00');
+
+  const timeOptions = [
+    { label: '21:58:00-20:10:00', value: '21:58:00-20:10:00' },
+    { label: 'Custom', value: 'Custom' },
+  ];
+
   const handlefiltersValueChange = (e) => {
     console.log(e.target.value);
     setFilters((filters) => ({ ...filters, [e.target.name]: e.target.value }));
   };
 
+  const handleChange = (e) => {
+
+    const valReceived = e.target.value;
+    setValue(valReceived);
+    timeOptions.push({ label: valReceived, value: valReceived });
+
+  }
+
   return (
-    <div>
+    <Box sx={{ flexGrow: 1 }}>
+
       <h1>Floor Map Analytics</h1>
       <div className="fma-content">
         <div className="fma-content-header">
@@ -78,23 +108,66 @@ export default function FloorMapAnalytics() {
                 >
                   {getOptions(frequencyOptions)}
                 </select>
+                {value !== 'Custom' ?
+                    <select value={value} onChange={handleChange}>
+                      {timeOptions.map((option) => (
+                        <option value={option.value}>{option.label}</option>
+                      ))}
+                    </select>
+                  :
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer
+                      components={['SingleInputTimeRangeField']}
+                    >
+                      <SingleInputTimeRangeField
+                        label="Time Selector"
+                        value={customTimeData}
+                        onChange={(newVal) => setCustomTimeData(newVal)}
+                        size='small'
+                        sx={{ lineHeight: '20px', padding: '0', width: 200 }}
+
+                      />
+                    </DemoContainer>
+
+                  </LocalizationProvider>
+                }
               </div>
 
-              {/* <select
-                    name="timeselector"
-                    value={area}
-                    onChange={handlefiltersValueChange}
-                  >
-                    {getOptions(areaOptions)}
-                  </select> */}
+              {/* {value !== 'Custom' ?
+                <select value={value} onChange={handleChange}>
+                  {timeOptions.map((option) => (
+                    <option value={option.value}>{option.label}</option>
+                  ))}
+                </select> :
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer
+                  components={['SingleInputTimeRangeField']}
+                >
+                  <SingleInputTimeRangeField 
+                  label="Time Selector" 
+                  value={customTimeData}
+                  onChange={(newVal) => setCustomTimeData(newVal)}
+                  size='small'
+                  sx={{ lineHeight: '20px', padding: '0', width: 200}}
+                  
+                  />
+                </DemoContainer>
+
+              </LocalizationProvider>
+              } */}
+
             </div>
             <>
               <CreateGraph selectedfrequency={frequency} selectedTime={time} />
             </>
           </div>
+
+
+
+
         </div>
       </div>
-    </div>
+    </Box>
   );
 }
 
