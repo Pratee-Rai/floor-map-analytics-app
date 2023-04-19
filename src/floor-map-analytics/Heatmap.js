35f8eea9d75fede2c5d0950b96fe0c40090d0f08
aravind.emmadishetty <aravind.emmadishetty@acsicorp.com>
2023-04-19 18:57:07 +0530

    added default date in filters

--- src/floor-map-analytics/Heatmap.js
+++ src/floor-map-analytics/Heatmap.js
@@ -6,9 +6,14 @@ import areaPath from "../images/icons/area-path.svg";
 import Video from "../components/video/Video";
 export default function Heatmap({ location, area }) {
   const [isWatching, setIsWatching] = useState(false);
+  const [isImgError, setIsImgError] = useState(false);
 
   const handleWatchNowClick = (e) => {
     setIsWatching(true);
+  };
+
+  const handleHeatmapImgError = (e) => {
+    setIsImgError(true);
   };
 
   const handleCloseVideo = (e) => {
@@ -17,7 +22,8 @@ export default function Heatmap({ location, area }) {
 
   useEffect(() => {
     setIsWatching(false);
-  }, [area]);
+    setIsImgError(false);
+  }, [area, location]);
 
   return (
     <div className="heatmap">
@@ -27,41 +33,44 @@ export default function Heatmap({ location, area }) {
           {location} - {area}
         </p>
       </div>
-      <div className="heatmap-content">
-        <div className="heatmap-img">
-          <>
-            {!isWatching ? (
-              <>
-                <img
-                  src={`./media/${location}/${area}/heatmap.png`}
-                  alt="Heat Map"
+      {!isImgError && (
+        <div className="heatmap-content">
+          <div className="heatmap-img">
+            <>
+              {!isWatching ? (
+                <>
+                  <img
+                    src={`./media/${location}/${area}/heatmap.png`}
+                    onError={handleHeatmapImgError}
+                    alt="Heat Map"
+                    height={434}
+                    width={572}
+                  />
+                  <button className="play-button" onClick={handleWatchNowClick}>
+                    Watch Video <img src={tvLogo} alt="tv logo" />
+                  </button>
+                </>
+              ) : (
+                <Video
                   height={434}
                   width={572}
-                />
-                <button className="play-button" onClick={handleWatchNowClick}>
-                  Watch Video <img src={tvLogo} alt="tv logo" />
-                </button>
-              </>
-            ) : (
-              <Video
-                height={434}
-                width={572}
-                autoPlay
-                showCloseButton
-                onClose={handleCloseVideo}
-              >
-                <source
-                  src={`./media/${location}/${area}/output-seg.mp4`}
-                  type="video/mp4"
-                />
-              </Video>
-            )}
-          </>
+                  autoPlay
+                  showCloseButton
+                  onClose={handleCloseVideo}
+                >
+                  <source
+                    src={`./media/${location}/${area}/output-seg.mp4`}
+                    type="video/mp4"
+                  />
+                </Video>
+              )}
+            </>
+          </div>
+          <div className="directions">
+            <img src={directionsIcon} alt="directions" />
+          </div>
         </div>
-        <div className="directions">
-          <img src={directionsIcon} alt="directions" />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
